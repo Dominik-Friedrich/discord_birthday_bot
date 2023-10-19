88d718363c054dc062b64827dc24f58a35073212
dfriedri <73181402+Dominik-Friedrich@users.noreply.github.com>
2023-10-19 14:15:21 +0200

    re-enabled other features

--- app/main.go
+++ app/main.go
@@ -1,31 +1,35 @@
 package main
 
 import (
+	log "github.com/chris-dot-exe/AwesomeLog"
 	"github.com/spf13/viper"
 	"main/src/bot"
+	"main/src/features/Birthday"
+	"main/src/features/Complaint"
 	"main/src/features/Player"
+	"main/src/lib/database"
 )
 
 func main() {
 	initViperEnv()
 
-	//db, err := database.NewConnection(
-	//	database.Config{
-	//		Type:     viper.GetString("database_type"),
-	//		Host:     viper.GetString("database_host"),
-	//		Port:     viper.GetString("database_port"),
-	//		User:     viper.GetString("database_user"),
-	//		Password: viper.GetString("database_password"),
-	//		Database: viper.GetString("database_database"),
-	//	})
-	//if err != nil {
-	//	log.Panicf("error initializing db connection: %v", err.Error())
-	//}
+	db, err := database.NewConnection(
+		database.Config{
+			Type:     viper.GetString("database_type"),
+			Host:     viper.GetString("database_host"),
+			Port:     viper.GetString("database_port"),
+			User:     viper.GetString("database_user"),
+			Password: viper.GetString("database_password"),
+			Database: viper.GetString("database_database"),
+		})
+	if err != nil {
+		log.Panicf("error initializing db connection: %v", err.Error())
+	}
 
 	birthdayBot := bot.NewBot(viper.GetString("discord_token"), viper.GetString("discord_application_id"))
 
-	//birthdayBot.RegisterFeature(Birthday.BirthdayRole(db))
-	//birthdayBot.RegisterFeature(Complaint.Complaint(db))
+	birthdayBot.RegisterFeature(Birthday.BirthdayRole(db))
+	birthdayBot.RegisterFeature(Complaint.Complaint(db))
 	birthdayBot.RegisterFeature(Player.Player())
 
 	birthdayBot.Run()
